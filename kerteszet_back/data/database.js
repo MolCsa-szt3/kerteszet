e4cb6c3014a65167137a646d1de29da216a4a1e9
szt3molcsa <szt3molcsa@vasvari.org>
2025-02-12 10:19:12 +0100

    base db functions are done

--- kerteszet_back/data/database.js
+++ kerteszet_back/data/database.js
@@ -1,10 +1,40 @@
 import sqlite from "sqlite3";
 
-const db = new sqlite.Database("plants");
+const db = new sqlite.Database("plants.sqlite");
 
 async function initializeDB() {
   console.log("InitializeDB is WIP");
+
+  dbRun("DROP TABLE IF EXISTS plants");
+  dbRun(
+    "CREATE TABLE IF NOT EXISTS plants (name STRING, perennial BOOLEAN, category STRING, price INTEGER)"
+  );
+
   return;
 }
 
+async function dbRun(sql, params = []) {
+  return new Promise((resolve, reject) => {
+    db.run(sql, params, (err) => {
+      if (err) reject(err);
+      else resolve(this);
+    });
+  });
+}
+async function dbGet(sql, params = []) {
+  return new Promise((resolve, reject) => {
+    db.get(sql, params, (err, row) => {
+      if (err) reject(err);
+      else resolve(row);
+    });
+  });
+}
+async function dbAll(sql, params = []) {
+  return new Promise((resolve, reject) => {
+    db.All(sql, params, (err, rows) => {
+      if (err) reject(err);
+      else resolve(rows);
+    });
+  });
+}
 export { initializeDB };
