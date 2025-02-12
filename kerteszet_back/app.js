3b08935e6d2a56018f3deaedb55ccbc26a818c58
szt3molcsa <szt3molcsa@vasvari.org>
2025-02-12 09:40:23 +0100

    minor fixes

--- kerteszet_back/app.js
+++ kerteszet_back/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import { initializeDB } from "./data/database.js";
 //import them routes
 
 const app = express();
@@ -11,5 +12,6 @@ app.use(express.json());
 //add routers
 
 app.listen(PORT, () => {
+  initializeDB();
   console.log(`Backend system is running on port ${PORT}`);
 });
--- kerteszet_back/data/database.js
+++ kerteszet_back/data/database.js
@@ -1,3 +1,10 @@
 import sqlite from "sqlite3";
 
 const db = new sqlite.Database("plants");
+
+async function initializeDB() {
+  console.log("InitializeDB is WIP");
+  return;
+}
+
+export { initializeDB };
