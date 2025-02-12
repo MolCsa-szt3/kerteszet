17b5b5c2fffa91605198df367c780f9a2c4570bc
szt3molcsa <szt3molcsa@vasvari.org>
2025-02-12 10:52:46 +0100

    post method to router

--- kerteszet_back/routers/plants.js
+++ kerteszet_back/routers/plants.js
@@ -11,4 +11,20 @@ router.get("/", async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
+router.post("/", async (req, res) => {
+  try {
+    const { name, isPerenial, category, price } = req.body;
+    if (!name || !isPerenial || !category || !price) {
+      return res.status(400).json({ message: "MIssing Data!" });
+    }
+    const data = dbRun(
+      "INSERT INTO plants (name, perennial, category, price) VALUES (?,?,?,?)",
+      [name, isPerenial, category, price]
+    );
+    return res.status(201).json({ message: "Plant Successfully Added" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+});
 export default router;
